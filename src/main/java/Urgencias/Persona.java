13ecfa7c802e4d31e1297f72c3ba2b6b9454cc8e
CENTIC <CENTIC@10.6.65.26>
2025-05-16 11:12:37 -0500

    cear clase persona

---
+++ src/main/java/Urgencias/Persona.java
@@ -0,0 +1,19 @@
+package Urgencias;
+
+
+public class Persona {
+    
+    private String nombre;
+    private String cedula;
+    private int edad;
+    private String genero;
+    
+    public Persona(){};
+    
+    
+    public Persona (String nombre,String cedula,int edad, String genero){
+        
+    }
+    
+    
+}
