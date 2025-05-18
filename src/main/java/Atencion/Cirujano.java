f84b0044c1e5aead6ff3a4062f2b5a9cf1ae87f8
Rafael <rafadz@gmail.com>
2025-05-17 21:24:05 -0500

    validacion de la clase cirujano

--- src/main/java/Atencion/Cirujano.java
+++ src/main/java/Atencion/Cirujano.java
@@ -4,4 +4,47 @@ package Atencion;
 
 public class Cirujano {
     
+    private String quirofano;
+    private String tipoProcedimiento; 
+    
+    public Cirujano(){}
+    
+    public Cirujano(String quirofano,String tipoProcedimiento){
+        
+        this.quirofano = quirofano;
+        this.tipoProcedimiento = tipoProcedimiento;
+    }
+    
+    public void setQuirfano(String quirofano){
+        if(quirofano == null || quirofano.isBlank() || quirofano.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")){ 
+            throw new IllegalArgumentException("El nombre es incorrecto");
+        }else{
+            this.quirofano = quirofano;
+        }
+    }
+    
+    public void setTipoProcedmiento(String tipoProcedimiento){
+       if(tipoProcedimiento == null || tipoProcedimiento.isBlank() || tipoProcedimiento.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")){ 
+            throw new IllegalArgumentException("El nombre es incorrecto");
+        }else{
+            this.tipoProcedimiento = tipoProcedimiento;
+        } 
+    }
+
+    public String getQuirofano() {
+        return quirofano;
+    }
+
+    public String getTipoProcedimiento() {
+        return tipoProcedimiento;
+    }
+    
+    @Override 
+    public String toString(){
+        
+        String str = ""; 
+        
+        return str;
+    }
+    
 }
