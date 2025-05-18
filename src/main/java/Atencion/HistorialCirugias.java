7a2b6e0ca9c8d65e1acb875680d7affc2636c91e
Rafael <rafadz@gmail.com>
2025-05-18 15:12:41 -0500

    creacion del historial de cirugias

--- src/main/java/Atencion/HistorialCirugias.java
+++ src/main/java/Atencion/HistorialCirugias.java
@@ -3,6 +3,53 @@ package Atencion;
 
 public class HistorialCirugias {
     
+    private String fechaProcedimientos;
+    private String horaProcedimiento;
+    private boolean exito; 
+    
+    public HistorialCirugias(){}
+    
+    public HistorialCirugias(String fechaProcedimiento,String horaProcedimiento){
+        this.fechaProcedimientos = fechaProcedimiento;
+        this.horaProcedimiento = horaProcedimiento; 
+    }
+    
+    public void setFechaProcedimientos(String fechaProcedimientos){
+       if(fechaProcedimientos == null || fechaProcedimientos.isBlank() || fechaProcedimientos.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")){ //Sirve para validar datos enteramente de letras y no de numeros, asi como mombres con acentuacion
+            throw new IllegalArgumentException("El nombre es incorrecto");
+        }else{
+            this.fechaProcedimientos = fechaProcedimientos;
+        }
+        
+        
+    }
+    
+    public void setHoraProcedimiento(String horaProcedimiento){
+        if(horaProcedimiento == null || horaProcedimiento.isBlank() || horaProcedimiento.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")){ //Sirve para validar datos enteramente de letras y no de numeros, asi como mombres con acentuacion
+            throw new IllegalArgumentException("El nombre es incorrecto");
+        }else{
+            this.horaProcedimiento = horaProcedimiento;
+        }
+    }
+
+    public String getFechaProcedimientos() {
+        return fechaProcedimientos;
+    }
+
+    public String getHoraProcedimiento() {
+        return horaProcedimiento;
+    }
+
+    
+    @Override 
+    
+    public String toString(){
+        
+        String str = "";
+        
+        return str; 
+        
+    }
     
     
     
